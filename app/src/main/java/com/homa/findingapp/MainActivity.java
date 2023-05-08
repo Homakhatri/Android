943472a8a59881259f5a1ecf0625a53b6ef770ac
Homakhatri <homakhatri221@gmail.com>
2023-05-08 09:14:50 +0545

    added new feature

--- app/src/main/java/com/homa/findingapp/MainActivity.java
+++ app/src/main/java/com/homa/findingapp/MainActivity.java
@@ -8,7 +8,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
+//This is new feature branch
 public class MainActivity extends AppCompatActivity {
     Button button;
     EditText editText;
